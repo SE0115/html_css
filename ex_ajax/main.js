d8586bbf6212ed7ab22a83b0946061b84152ae62
sieun <sieun0115@ajou.ac.kr>
2021-10-26 16:47:05 +0900

    목록 ajax로 구현하기

--- ex_ajax/main.js
+++ ex_ajax/main.js
@@ -10,3 +10,14 @@ function Click(fileName) {
     });
     // alert('hi');
 }
+fetch('list').then((res) => {
+    const ulEl = document.createElement('ul');
+    res.text().then((text) => {
+        text.split(',').forEach((el) => {
+            const liEl = document.createElement('li');
+            liEl.innerHTML = `<a href="#!${el}" onclick="Click('${el}')">${el}</a>`
+            ulEl.appendChild(liEl);
+        })
+        document.body.prepend(ulEl);
+    });
+})
